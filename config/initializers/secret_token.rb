7fb3bb04f6248fa40ff1fa3e8adfa8a330f023d8
Nick Fox <nicholas.f.fox@gmail.com>
2013-07-18 18:18:28 -0400

    Created Project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HdcpTracker::Application.config.secret_token = '4675333954b2d14ebf5877c604cf251a1d7a579a9c61e10b4a13c7f10355c827b47010b281c49a0eaad0ae53b9aff80a8f8266c06dcc099b76d3a52f561dda06'

d45c95e3a1b507634a47bf8509d6c0da988b0556
Nick Fox <nicholas.f.fox@gmail.com>
2013-08-15 22:56:53 -0400

    Fixed password mailer host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,7 +66,7 @@ HdcpTracker::Application.configure do
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
 
 
-  config.action_mailer.default_url_options = { :host => 'herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => 'nickfox-usga-hdcp-tracker.herokuapp.com' }
   # ActionMailer Config
   # Setup for production - deliveries, no errors raised
   config.action_mailer.delivery_method = :smtp
